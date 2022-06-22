02d0a6de7bcbe88836787899de3d696dd0619e7d
Ablyamitov <ablyamitov2003@mail.ru>
2022-06-22 21:10:23 +0300

    add pressing effect

--- Calculator/mainwindow.cpp
+++ Calculator/mainwindow.cpp
@@ -13,3 +13,368 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_facButt_released()
+{
+    ui->facFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_perButt_released()
+{
+    ui->perFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_degreeButt_released()
+{
+    ui->degreeFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_sqrtButt_released()
+{
+    ui->sqrtFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_logButt_released()
+{
+    ui->logFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_piButt_released()
+{
+    ui->piFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_expButt_released()
+{
+    ui->expFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_sinButt_released()
+{
+    ui->sinFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_cosButt_released()
+{
+    ui->cosFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_tanButt_released()
+{
+    ui->tanFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_cotButt_released()
+{
+    ui->cotFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_plusButt_released()
+{
+    ui->plusFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_minusButt_released()
+{
+    ui->minusFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_multButt_released()
+{
+    ui->multFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_divButt_released()
+{
+    ui->divFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+
+
+
+void MainWindow::on_equalButt_released()
+{
+    ui->equalFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+
+
+void MainWindow::on_reverseButt_released()
+{
+    ui->reverseFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_clearAllButt_released()
+{
+    ui->clearAllFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_clearButt_released()
+{
+    ui->clearFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_1_released()
+{
+    ui->numFrame_1->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_2_released()
+{
+    ui->numFrame_2->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_3_released()
+{
+    ui->numFrame_3->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_4_released()
+{
+    ui->numFrame_4->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_5_released()
+{
+    ui->numFrame_5->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_6_released()
+{
+    ui->numFrame_6->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_7_released()
+{
+    ui->numFrame_7->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_8_released()
+{
+    ui->numFrame_8->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_9_released()
+{
+    ui->numFrame_9->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_numButt_0_released()
+{
+    ui->numFrame_0->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_dotButt_released()
+{
+    ui->dotFrame->setStyleSheet(defaultFrameStyle);
+}
+
+
+void MainWindow::on_facButt_pressed()
+{
+    ui->facFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_perButt_pressed()
+{
+    ui->perFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_degreeButt_pressed()
+{
+    ui->degreeFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_sqrtButt_pressed()
+{
+    ui->sqrtFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_logButt_pressed()
+{
+    ui->logFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_piButt_pressed()
+{
+    ui->piFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_expButt_pressed()
+{
+    ui->expFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_sinButt_pressed()
+{
+    ui->sinFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_cosButt_pressed()
+{
+    ui->cosFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_tanButt_pressed()
+{
+    ui->tanFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_cotButt_pressed()
+{
+    ui->cotFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_plusButt_pressed()
+{
+    ui->plusFrame->setStyleSheet(commandsButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_minusButt_pressed()
+{
+    ui->minusFrame->setStyleSheet(commandsButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_multButt_pressed()
+{
+    ui->multFrame->setStyleSheet(commandsButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_divButt_pressed()
+{
+    ui->divFrame->setStyleSheet(commandsButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_equalButt_pressed()
+{
+    ui->equalFrame->setStyleSheet(equalButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_reverseButt_pressed()
+{
+    ui->reverseFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_clearAllButt_pressed()
+{
+    ui->clearAllFrame->setStyleSheet(clearButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_clearButt_pressed()
+{
+    ui->clearFrame->setStyleSheet(clearButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_1_pressed()
+{
+    ui->numFrame_1->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_2_pressed()
+{
+    ui->numFrame_2->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_3_pressed()
+{
+    ui->numFrame_3->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_4_pressed()
+{
+    ui->numFrame_4->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_5_pressed()
+{
+    ui->numFrame_5->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_6_pressed()
+{
+    ui->numFrame_6->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_7_pressed()
+{
+    ui->numFrame_7->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_8_pressed()
+{
+    ui->numFrame_8->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_9_pressed()
+{
+    ui->numFrame_9->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_numButt_0_pressed()
+{
+    ui->numFrame_0->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
+
+void MainWindow::on_dotButt_pressed()
+{
+    ui->dotFrame->setStyleSheet(enginButtonFrameFocusStyle);
+}
+
--- Calculator/mainwindow.h
+++ Calculator/mainwindow.h
@@ -20,8 +20,173 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_facButt_pressed();
+
+    void on_facButt_released();
+
+    void on_perButt_pressed();
+
+    void on_perButt_released();
+
+    void on_degreeButt_released();
+
+    void on_degreeButt_pressed();
+
+    void on_sqrtButt_pressed();
+
+    void on_logButt_pressed();
+
+    void on_piButt_pressed();
+
+    void on_expButt_pressed();
+
+    void on_sinButt_pressed();
+
+    void on_cosButt_pressed();
+
+    void on_tanButt_pressed();
+
+    void on_cotButt_pressed();
+
+    void on_sqrtButt_released();
+
+    void on_logButt_released();
+
+    void on_piButt_released();
+
+    void on_expButt_released();
+
+    void on_sinButt_released();
+
+    void on_cosButt_released();
+
+    void on_tanButt_released();
+
+    void on_cotButt_released();
+
+    void on_clearButt_pressed();
+
+    void on_plusButt_pressed();
+
+    void on_minusButt_pressed();
+
+    void on_multButt_pressed();
+
+    void on_divButt_pressed();
+
+    void on_equalButt_pressed();
+
+    void on_clearButt_released();
+
+    void on_plusButt_released();
+
+    void on_minusButt_released();
+
+    void on_multButt_released();
+
+    void on_divButt_released();
+
+    void on_equalButt_released();
+
+    void on_reverseButt_pressed();
+
+    void on_reverseButt_released();
+
+    void on_clearAllButt_pressed();
+
+    void on_clearAllButt_released();
+
+    void on_numButt_1_released();
+
+    void on_numButt_1_pressed();
+
+    void on_numButt_2_pressed();
+
+    void on_numButt_3_pressed();
+
+    void on_numButt_4_pressed();
+
+    void on_numButt_5_pressed();
+
+    void on_numButt_6_pressed();
+
+    void on_numButt_7_pressed();
+
+    void on_numButt_8_pressed();
+
+    void on_numButt_9_pressed();
+
+    void on_numButt_0_pressed();
+
+    void on_dotButt_pressed();
+
+    void on_numButt_2_released();
+
+    void on_numButt_3_released();
+
+    void on_numButt_4_released();
+
+    void on_numButt_5_released();
+
+    void on_numButt_6_released();
+
+    void on_numButt_7_released();
+
+    void on_numButt_8_released();
+
+    void on_numButt_9_released();
+
+    void on_numButt_0_released();
+
+    void on_dotButt_released();
 
 private:
     Ui::MainWindow *ui;
+
+    QString equalButtonFrameFocusStyle = "QFrame{"
+            "border-style: solid;"
+            "border-width: 5px;"
+            "border-color: rgb(147,190,251);"
+            "border-radius: 6px;"
+            "background-color: rgb(147,190,251);"
+            "}";
+
+    QString clearButtonFrameFocusStyle = "QFrame{"
+            "border-style: solid;"
+            "border-width: 5px;"
+            "border-color: rgb(240, 168, 175);"
+            "border-radius: 6px;"
+            "background-color: rgb(240, 168, 175);"
+            "}";
+
+    QString commandsButtonFrameFocusStyle = "QFrame{"
+            "border-style: solid;"
+            "border-width: 5px;"
+            "border-color: rgb(157, 204, 182);"
+            "border-radius: 6px;"
+            "background-color: rgb(157, 204, 182);"
+            "}";
+
+    QString enginButtonFrameFocusStyle = "QFrame{"
+            "border-style: solid;"
+            "border-width: 5px;"
+            "border-color: rgb(181,185,189);"
+            "border-radius: 6px;"
+            "background-color: rgb(181,185,189);"
+            "}";
+
+    QString defaultFrameStyle = "QFrame{"
+            "border: none;"
+            "background-color: white;"
+            "}";
+
+    QString exitButtonFrameFocusStyle = "QFrame{"
+            "border-style: solid;"
+            "border-width: 5px;"
+            "border-color: rgb(192,196,200);"
+            "border-radius: 6px;"
+            "background-color: rgb(192,196,200);"
+            "}";
+
 };
 #endif // MAINWINDOW_H
