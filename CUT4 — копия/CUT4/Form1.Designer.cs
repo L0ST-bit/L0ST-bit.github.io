26ffc46fa716d51270e755c4544297e15eada428
L0ST-bit <strigius2013@gmail.com>
2023-04-11 19:31:36 +0300

    1

---
+++ CUT4 — копия/CUT4/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CUT4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.frequencyRangeTextBox = new System.Windows.Forms.TextBox();
+            this.lTextBox = new System.Windows.Forms.TextBox();
+            this.firstAntennaHightLabel = new System.Windows.Forms.Label();
+            this.lastAntennaHightLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1281, 844);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1333, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frequencyRangeTextBox
+            // 
+            this.frequencyRangeTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.frequencyRangeTextBox.Location = new System.Drawing.Point(1333, 68);
+            this.frequencyRangeTextBox.Name = "frequencyRangeTextBox";
+            this.frequencyRangeTextBox.Size = new System.Drawing.Size(100, 31);
+            this.frequencyRangeTextBox.TabIndex = 2;
+            this.frequencyRangeTextBox.Text = "160";
+            // 
+            // lTextBox
+            // 
+            this.lTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lTextBox.Location = new System.Drawing.Point(1333, 105);
+            this.lTextBox.Name = "lTextBox";
+            this.lTextBox.Size = new System.Drawing.Size(100, 31);
+            this.lTextBox.TabIndex = 3;
+            this.lTextBox.Text = "40";
+            // 
+            // firstAntennaHightLabel
+            // 
+            this.firstAntennaHightLabel.AutoSize = true;
+            this.firstAntennaHightLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstAntennaHightLabel.Location = new System.Drawing.Point(1328, 149);
+            this.firstAntennaHightLabel.Name = "firstAntennaHightLabel";
+            this.firstAntennaHightLabel.Size = new System.Drawing.Size(0, 25);
+            this.firstAntennaHightLabel.TabIndex = 4;
+            // 
+            // lastAntennaHightLabel
+            // 
+            this.lastAntennaHightLabel.AutoSize = true;
+            this.lastAntennaHightLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lastAntennaHightLabel.Location = new System.Drawing.Point(1328, 185);
+            this.lastAntennaHightLabel.Name = "lastAntennaHightLabel";
+            this.lastAntennaHightLabel.Size = new System.Drawing.Size(0, 25);
+            this.lastAntennaHightLabel.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1439, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "МГц";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1439, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "км";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lastAntennaHightLabel);
+            this.Controls.Add(this.firstAntennaHightLabel);
+            this.Controls.Add(this.lTextBox);
+            this.Controls.Add(this.frequencyRangeTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox frequencyRangeTextBox;
+        private System.Windows.Forms.TextBox lTextBox;
+        private System.Windows.Forms.Label firstAntennaHightLabel;
+        private System.Windows.Forms.Label lastAntennaHightLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CUT4 — копия/CUT4/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot;
+
+
+namespace CUT4
+{
+    public partial class Form1 : Form
+    {
+        private double frequencyRange; 
+        private static double L;
+        private readonly int minHeight = 10, maxHeight = 100;
+
+        List<double> k_FrenilZoneRelativeAntennaHeights = new List<double>();
+        List<double> k_list = new List<double>();
+
+        List<double> Earth = new List<double>();
+        List<double> k_Earth = new List<double>();
+
+        List<double> Height_ = new List<double>();
+        List<double> x_ = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void setValues()
+        {
+            try
+            {
+                L = double.Parse(lTextBox.Text) * 1e3;
+                frequencyRange = double.Parse(frequencyRangeTextBox.Text) * 1e6;
+            }
+            catch(Exception)
+            {
+                L = 40 * 1e3;
+                frequencyRange = 160 * 1e6;
+            }
+
+
+        }
+        //Случайные значения для рельефа
+        private List<int> getRandomDeltaHeightsList(List<int> deltaHeightsList, int minHeight, int maxHeight)
+        {
+
+            Random random = new Random();
+            for (double i = 0; i <= 10; i++)
+            {
+                deltaHeightsList.Add(random.Next(minHeight, maxHeight));
+            }
+
+            return deltaHeightsList;
+        }
+
+        //Высоты рельефа
+        private List<double> getRandomHeightsList(List<double> heightsList, List<double> earthsCurvatureList)
+        {
+            List<int> deltaHeightsList = new List<int>();
+            deltaHeightsList = getRandomDeltaHeightsList(deltaHeightsList, minHeight, maxHeight);
+            for (int i = 0; i <= 10; i++)
+            {
+                double x = i / 10d;
+                double height = deltaHeightsList[i] + earthsCurvatureList[i];
+                heightsList.Add(height);
+
+                Height_.Add(height);
+                
+                x_.Add(x);
+            }
+            return heightsList;
+        }
+
+        //Рельеф поверхности
+        private double getY(double k)
+        {
+            double R = 8500d * 1e3;
+
+            double y = ((L * L) / (2 * R)) * k * (1 - k);
+            return y;
+        }
+
+        //Кривизна земли
+        private List<double> getEarthsCurvatureList(List<double> earthsCurvatureList)
+        {
+            for (double x = 0; x <= L; x += L / 10d)
+            {
+                double k = x / L;
+                double y = getY(k);
+                earthsCurvatureList.Add(y);
+                Earth.Add(y);
+                k_Earth.Add(k);
+            }
+            return earthsCurvatureList;
+        }
+
+
+        //Зона Френеля
+        private List<double> getMinimumZone(List<double> FrenilZone, List<double> heightsList)
+        {
+            double c = 299792458;
+            double Alpha = c / frequencyRange;
+            for (double x = 0; x <= L; x += L / 10d)
+            {
+                double k = x / L;
+                double xMinimumZone = Math.Sqrt((L * Alpha * k * (1 - k)) / 3d);
+                FrenilZone.Add(xMinimumZone);
+            }
+
+
+            List<double> FrenilZoneRelativeAntennaHeights = new List<double>();
+
+            for (double x = 0; x <= L; x += L / 10d)
+            {
+                double k = x / L;
+                int i = (int)(10 * k);
+                double xMinimumZoneRelativeAntennaHeights = heightsList.Max() - FrenilZone[i] + FrenilZone.Max() - FrenilZone.Min();
+                FrenilZoneRelativeAntennaHeights.Add(xMinimumZoneRelativeAntennaHeights);
+            }
+            double minDistanceFrenilZoneToHeight = FrenilZoneRelativeAntennaHeights.First() - heightsList.First();
+            double curDistance;
+            for (int i = 0; i <= 10; i++)
+            {
+                curDistance = FrenilZoneRelativeAntennaHeights[i] - heightsList[i];
+                if (curDistance < minDistanceFrenilZoneToHeight) minDistanceFrenilZoneToHeight = curDistance;
+            }
+
+
+            for (int i = 0; i < FrenilZoneRelativeAntennaHeights.Count; i++)
+            {
+                double k = i * 0.1;
+                FrenilZoneRelativeAntennaHeights[i] -= minDistanceFrenilZoneToHeight;
+                k_FrenilZoneRelativeAntennaHeights.Add(FrenilZoneRelativeAntennaHeights[i]);
+                k_list.Add(k);
+
+            }
+            return FrenilZoneRelativeAntennaHeights;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k_FrenilZoneRelativeAntennaHeights.Clear();
+            k_list.Clear();
+
+            Earth.Clear();
+            k_Earth.Clear();
+
+            Height_.Clear();
+            x_.Clear();
+
+
+
+            setValues();
+            firstAntennaHightLabel.Text = "Высота 1й мачты антенны: ";
+            lastAntennaHightLabel.Text = "Высота 2й мачты антенны: ";
+
+            List<double> earthsCurvatureList = new List<double>();
+            earthsCurvatureList = getEarthsCurvatureList(earthsCurvatureList);
+
+
+            List<double> heightsList = new List<double>();
+            heightsList = getRandomHeightsList(heightsList, earthsCurvatureList);
+
+
+            List<double> FrenilZone = new List<double>();
+
+            FrenilZone = getMinimumZone(FrenilZone, heightsList);
+
+            var plot = new ScottPlot.Plot(965, 680);
+            plot.Style(Style.Gray1);
+            plot.Palette = Palette.Category20;
+            plot.Title("Профиль трассы РРЛ");
+            plot.XLabel("k");
+            plot.YLabel("h, м");
+
+            double[] value = new double[] { 0, 1 };
+            double[] value_min = new double[] { FrenilZone.First(), FrenilZone.Last() };
+
+            plot.AddLine(0, FrenilZone.First(), 0, heightsList.First());
+            plot.AddLine(1, FrenilZone.Last(), 1, heightsList.Last());
+            plot.AddScatter(x_.ToArray(), Height_.ToArray(),color: Color.Aqua ,  label: "Высоты рельефа");
+            plot.AddScatterLines(k_Earth.ToArray(), Earth.ToArray(), label: "Кривизна Земли");
+            plot.AddScatterLines(k_list.ToArray(), k_FrenilZoneRelativeAntennaHeights.ToArray(), label: "Минимальная зона");
+            plot.AddScatterLines(value, value_min, lineStyle: LineStyle.Dash, label:"Линия прямой видимости");
+            plot.Legend();
+
+            double firstAntennaHeight = Math.Ceiling(FrenilZone.First() - heightsList.First());
+            double lastAntennaHeight = Math.Ceiling(FrenilZone.Last() - heightsList.Last());
+            firstAntennaHightLabel.Text +=  firstAntennaHeight.ToString() + " м";
+            lastAntennaHightLabel.Text += lastAntennaHeight.ToString() + " м";
+
+            plot.SaveFig("1.png");
+            pictureBox1.ImageLocation = "1.png";
+        }
+    }
+}
